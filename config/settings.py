86016d3ce38cb7b3ed85eba5092909627edbfe36
Max Barulin <barulin.max@gmail.com>
2025-06-01 23:11:23 +0300

    Create app users, models users

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     'mailing_service',
     'users',
@@ -140,10 +141,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# AUTH_USER_MODEL = 'users.CustomUser'
-#
-# LOGIN_REDIRECT_URL = 'catalog:show_home'
-# LOGIN_URL = 'users:login'
+AUTH_USER_MODEL = 'users.CustomUser'
+
+LOGIN_REDIRECT_URL = 'mailing_service:home'
+LOGIN_URL = 'users:login'
+LOGOUT_REDIRECT_URL = 'mailing_service:home'
 
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
 EMAIL_HOST = os.getenv('EMAIL_HOST')
@@ -162,3 +164,5 @@ if CACHE_ENABLED:
             'LOCATION': os.getenv('LOCATION'),
         }
     }
+
+SITE_ID = 1
